84fcf399eca888ca4599f5b7e45abf56a95c0fc0
Daniel Adam <bubba.adam@gmail.com>
2012-09-28 19:42:37 -0400

    First commit

---
+++ GCA3/GCA3/GCA3/Sprite.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace GCA3
+{
+
+    public class Sprite
+    {
+
+        protected Vector2 position { get; set; }
+        protected Vector2 veloctiy { get; set; }
+        protected Texture2D image { get; set; }
+        
+
+        
+
+
+        public Sprite(Game game)
+        {
+
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
