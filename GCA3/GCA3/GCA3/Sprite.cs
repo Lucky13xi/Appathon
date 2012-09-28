3980f78db0b4a02eb22dce1c6cd7875d7190260a
Daniel Adam <bubba.adam@gmail.com>
2012-09-28 19:58:52 -0400

    Update 1

--- GCA3/GCA3/GCA3/Sprite.cs
+++ GCA3/GCA3/GCA3/Sprite.cs
@@ -18,14 +18,21 @@ namespace GCA3
 
         protected Vector2 position { get; set; }
         protected Vector2 veloctiy { get; set; }
+        protected double rotation { get; set; }
         protected Texture2D image { get; set; }
+        protected float scale { get; set; }
+        protected Rectangle boundingBox { get; set; }
+        protected SoundEffect hitSound { get; set; }
+        protected bool alive { get; set; }
         
-
-        
-
-
-        public Sprite(Game game)
+        public void Sprite(GraphicsDevice graphics, Vector2 _position, Vector2 _velocity, double _rotation, Texture2D _image, float _scale, SoundEffect _hitSound, bool _alive)
         {
+
+            position = _position;
+            veloctiy = _velocity;
+            rotation = _rotation;
+
+
 
         }
 
