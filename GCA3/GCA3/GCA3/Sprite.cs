276112a7c9213c772918c0020231a74d71d3144b
Daniel Adam <bubba.adam@gmail.com>
2012-09-28 20:41:18 -0400

    UPDATE!

--- GCA3/GCA3/GCA3/Sprite.cs
+++ GCA3/GCA3/GCA3/Sprite.cs
@@ -24,16 +24,21 @@ namespace GCA3
         protected Rectangle boundingBox { get; set; }
         protected SoundEffect hitSound { get; set; }
         protected bool alive { get; set; }
-        
-        public void Sprite(GraphicsDevice graphics, Vector2 _position, Vector2 _velocity, double _rotation, Texture2D _image, float _scale, SoundEffect _hitSound, bool _alive)
-        {
+        protected int windowWidth { get; set; }
+        protected int windowHeight { get; set; }
 
+
+        public Sprite(GraphicsDevice graphics, Vector2 _position, Vector2 _velocity, double _rotation, Texture2D _image, float _scale, SoundEffect _hitSound, bool _alive, int _windowWidth, int _windowHeight)
+        {
             position = _position;
             veloctiy = _velocity;
             rotation = _rotation;
-
-
-
+            image = _image;
+            scale = _scale;
+            hitSound = _hitSound;
+            alive = _alive;
+            windowHeight = _windowHeight;
+            windowWidth = _windowWidth;
         }
 
 
@@ -41,5 +46,14 @@ namespace GCA3
         {
 
         }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (alive)
+            {
+                spriteBatch.Draw(image, position, null, Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, 0);
+            }
+        }
+
     }
 }
---
+++ GCA3/GCA3/GCA3/SpriteAnimation.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace GCA3
+{
+    public class SpriteAnimation : Sprite
+    {
+        public SpriteAnimation(GraphicsDevice graphics, Vector2 _position, Vector2 _velocity, double _rotation, Texture2D _image, float _scale, SoundEffect _hitSound, bool _alive, int _windowWidth, int _windowHeight)
+            :base (graphics, _position, _velocity, _rotation, _image, _scale, _hitSound, _alive, _windowWidth, _windowHeight)
+        {
+
+        }
+
+        public override Draw()
+        {
+        }
+    }
+}
